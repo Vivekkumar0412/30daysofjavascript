cb139b846390b0aacf81ca707d2be13179967fe7
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-05 00:24:24 +0530

    new question added

--- index.js
+++ index.js
@@ -3,9 +3,17 @@
 // program to swap two numbers
 let a = 15;
 let b = 6;
-let c;
-c = b;
-b = a;
-a = c;
+// let c;
+// c = b;
+// b = a;
+// a = c;
+// console.log(a);
+// console.log(b);
+
+// program to swap two numbers without using 3rd varriable
+
+a = a+b;
+b = a-b;
+a = a-b;
 console.log(a);
 console.log(b);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
-
-// console.log(typeof(Myname)); 
+);
+// console.log(b);
