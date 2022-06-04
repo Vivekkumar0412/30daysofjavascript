c24c39dc0cd02911b6b1fe45bc01eb723cd84bac
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-05 00:07:02 +0530

    #30daysofjavascript started

---
+++ index.js
@@ -0,0 +1,11 @@
+// var Myname = "Vivek kumar";
+// console.log(typeof(Myname)); 
+// program to swap two numbers
+let a = 15;
+let b = 6;
+let c;
+c = b;
+b = a;
+a = c;
+console.log(a);
+console.log(b);
